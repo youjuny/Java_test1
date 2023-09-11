538b543c0a90cf13ec6ea6f4716b0ac1a6ad94e2
youjuny <youyoung2774@gmail.com>
2023-09-11 17:08:57 +0900

    자바 기초 체트스2

--- src/main/java/Exam2.java
+++ src/main/java/Exam2.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Exam2 {
+public class Exam2 {
+
+    public static void main(String[] args) {
+        삼각형(3);
+        System.out.println();
+        삼각형(5);
+        System.out.println();
+        삼각형(7);
+    }
+
+
+    public static void 삼각형(int h) {
+        for (int i = 1; i <= h; i++) {
+
+            for (int j = 1; j <= h - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+
 }
--- src/main/java/Exam3.java
+++ src/main/java/Exam3.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Exam3 {
+
+
+
+public class Exam3 {
+
+    public static void main(String[] args) {
+
+        청둥오리 a청둥오리 = new 청둥오리(); // 청둥오리가 만들어집니다. 청둥오리는 기본 날개를 가지고 태어납니다.
+
+        a청둥오리.날다(); // 청둥오리가 날개로 천천히 날아갑니다.
+
+        흰오리 a흰오리 = new 흰오리(); // 흰오리가 만들어집니다. 흰오리는 불사조 날개를 가지고 태어납니다.
+
+        a흰오리.날다(); // 흰오리가 불사조 날개로 불타오르며 날아갑니다.
+
+        붉은오리 a붉은오리 = new 붉은오리(); // 붉은오리가 만들어집니다. 붉은오리는 로켓 날개를 가지고 태어납니다.
+
+        a붉은오리.날다(); // 붉은오리가 로켓날개로 놀라운 속도로 굉음을 내며 날아갑니다
+
+    }}
+
+class 오리 {
+
+}
+
+
+
+class 청둥오리 extends 오리{
+
+    청둥오리(){
+        System.out.println("청둥오리가 만들어집니다. 청둥오리는 기본날개를 가지고 태어납니다.");
+    }
+    public void 날다() {
+        System.out.println("청둥오리가 날개로 천천히 날아갑니다.");
+    }
+}
+
+class 흰오리 extends 오리{
+
+    흰오리() {
+        System.out.println("흰오리가 만들어집니다. 흰오리는 불사조 날개를 가지고 태어납니다.");
+    }
+
+
+    public void 날다() {
+        System.out.println("흰오리가 불사조 날개로 불타오르며 날아갑니다.");
+    }
+}
+
+class 붉은오리 extends 오리{
+
+    붉은오리(){
+        System.out.println("붉은오리가 만들어집니다. 붉은오리는 로켓 날개를 가지고 태어납니다.");
+    }
+    public void 날다() {
+        System.out.println("붉은오리가 로켓 날개로 놀라운 속도로 굉음을 내며 날아갑니다.");
+    }
 }
